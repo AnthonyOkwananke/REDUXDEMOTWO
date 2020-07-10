c2d5610387751a4ec6c3a8169ab780ae94e77e74
Anthony Okwananke <826717@ACDM.DS.SAIT.CA>
2020-07-10 08:52:36 -0600

    Added react to the redux base project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useSelector, useDispatch } from 'react-redux'; //to display counter
+import { increment } from './actions';
+import { decrement } from './actions';
 
 function App() {
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector(state => state.isLogged);
+  const dispatch = useDispatch();
   return (
     <div className="App">
-      <h1>hello</h1>
+      <h1>Counter {counter}</h1>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      {isLogged ? <h3>Valuable information. Not for your eyes.</h3> : ''}
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,8 +4,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux';
+import allReducers from './reducers';
+import { Provider } from 'react-redux';
 
-//Action - INCREMENT
+const store = createStore(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+/* //Action - INCREMENT
 const increment = () => {
   return {
     type: 'INCREMENT'
@@ -36,12 +40,15 @@ store.subscribe(() => console.log(store.getState()));
 
 store.dispatch(increment())
 store.dispatch(decrement())
-store.dispatch(decrement())
+store.dispatch(decrement()) */
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
+
   document.getElementById('root')
 );
 
